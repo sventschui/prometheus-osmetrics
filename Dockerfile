dbd10dd2f48c1e153b405ef24f973948c125691c
Sven Tschui <sventschui@gmail.com>
2020-04-23 14:34:54 +0200

    Add publish script, improve docker image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,28 @@
-FROM node:13.13.0
+FROM node:13.13.0-alpine as build
 
 WORKDIR /nodeapp
 
 COPY package.json yarn.lock ./
 
-RUN yarn --frozen-lockfile --production
+ENV NODE_ENV=production
+
+RUN yarn --frozen-lockfile --production --no-bin-links --non-interactive --cache-folder /tmp/ycache
 
 COPY src ./src/
 
-RUN ls -alh .
+COPY docker-entrypoint.sh /nodeapp
+
+FROM node:13.13.0-alpine
+
+WORKDIR /nodeapp
+
+ENTRYPOINT ["/nodeapp/docker-entrypoint.sh"]
+
+CMD ["start"]
+
+RUN adduser -D -H -s /bin/false prometheus-osmetrics \
+  && chown prometheus-osmetrics:prometheus-osmetrics .
+
+USER prometheus-osmetrics
+
+COPY --from=build --chown="prometheus-osmetrics:prometheus-osmetrics" /nodeapp /nodeapp
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+const pino = require('pino')
 const createServer = require('./server')
 const collectSettings = require('./collect-settings')
 
+const logger = pino({
+  prettyPrint: process.env.NODE_ENV != 'production' ? {
+    translateTime: true
+  } : false,
+});
+
+console.log(process.argv)
+
 const start = async () => {
-  const { osApi, osMetricApi, accessToken } = await collectSettings(process.env)
+  let options;
+  try {
+  options = await collectSettings(process.env);
+  } catch (e) {
+    pino.final(logger).error(e.message);
+    process.exit(-1);
+  }
 
   const server = createServer({
-    fastifyOptions: {
-      logger: {
-        prettyPrint: {
-          translateTime: true
-        }
-      }
-    },
-    osApi,
-    osMetricApi,
-    accessToken
+    logger,
+    ...options,
   })
 
-  server.log.info('Using OS API %s, OS Metric API %s', osApi, osMetricApi)
+  server.log.info('Using OS API %s, OS Metric API %s', options.osApi, options.osMetricApi)
 
   try {
     await server.listen(3000, '0.0.0.0')
